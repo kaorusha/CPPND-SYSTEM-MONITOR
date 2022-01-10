17b3cba55e02711a6f5f7078c5b5b3d93a754d2e
kaorusha <cindy_hsieh@avc.co>
2022-01-10 19:19:43 +0800

    feat: add formater include file

--- src/format.cpp
+++ src/format.cpp
@@ -1,5 +1,5 @@
 #include <string>
-
+#include <chrono>
 #include "format.h"
 
 using std::string;
@@ -8,4 +8,6 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds[[maybe_unused]]) {
+  auto h = std::chrono::duration_cast<
+  return string(); }
